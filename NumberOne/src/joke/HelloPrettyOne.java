3eb188ec4787b59604c551a7c5739c83fa771e80
Yuriy Shestakov <y.shestakov@me.com>
2016-03-21 13:43:41 +0300

    This is a commit

---
+++ NumberOne/src/joke/HelloPrettyOne.java
@@ -0,0 +1,11 @@
+package joke;
+
+public class HelloPrettyOne {
+
+	public static void main(String[] args) {
+		String greet = "You are the most beautiful woman in the galaxy";
+		System.out.println(greet);
+
+	}
+
+}
---
+++ NumberOne/src/joke2/MyClas.java
@@ -0,0 +1,5 @@
+package joke2;
+
+public class MyClas {
+
+}
